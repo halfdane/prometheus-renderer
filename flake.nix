2e1e1f946a86335f801fdefb5893d5c935af4776
halfdane <halfdane@gmx.net>
2026-03-01 17:51:11 +0100

    chore: bump version to v0.1.1

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
 
   outputs = { self, nixpkgs, flake-utils }:
     let
-      version = "0.1.0";
+      version = "0.1.1";
     in
     flake-utils.lib.eachSystem [ "aarch64-linux" "x86_64-linux" ] (system:
       let
