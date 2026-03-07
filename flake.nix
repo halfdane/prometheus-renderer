9bcf1dbd0e5f81008cd2be4518f2a4333d9ba59a
halfdane <halfdane@gmx.net>
2026-03-07 12:15:25 +0100

    chore: bump version to v0.2.2

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
       in
       {
         packages.default =
-          let renderVersion = "0.2.1"; in
+          let renderVersion = "0.2.2"; in
           pkgs.buildGoModule {
             pname = "prometheus-render";
             version = renderVersion;
